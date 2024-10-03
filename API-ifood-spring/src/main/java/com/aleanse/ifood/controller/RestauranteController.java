967c074e02f4c8ad5683eb2575251ff14ae48e3b
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-10-03 01:38:44 -0300

    implementado criação de restaurante

--- API-ifood-spring/src/main/java/com/aleanse/ifood/controller/RestauranteController.java
+++ API-ifood-spring/src/main/java/com/aleanse/ifood/controller/RestauranteController.java
@@ -1,6 +1,7 @@
 package com.aleanse.ifood.controller;
 
 
+import com.aleanse.ifood.exception.EntidadeNaoEncontradaException;
 import com.aleanse.ifood.model.Restaurante;
 import com.aleanse.ifood.repository.RestauranteRepository;
 import com.aleanse.ifood.service.CadastroRestauranteService;
@@ -35,8 +36,15 @@ public class RestauranteController {
         }
     }
     @PostMapping
-    public ResponseEntity<Restaurante> salvar(@RequestBody Restaurante restaurante){
-        return ResponseEntity.status(HttpStatus.CREATED).body(cadastroRestauranteService.salvarRestaurante(restaurante));
+    public ResponseEntity<?> salvar(@RequestBody Restaurante restaurante){
+        try {
+            Restaurante restauranteSalvo = cadastroRestauranteService.salvarRestaurante(restaurante);
+            return ResponseEntity.status(HttpStatus.CREATED).body(restauranteSalvo);
+        } catch (EntidadeNaoEncontradaException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
+
     }
 
 
--- API-ifood-spring/src/main/java/com/aleanse/ifood/service/CadastroRestauranteService.java
+++ API-ifood-spring/src/main/java/com/aleanse/ifood/service/CadastroRestauranteService.java
@@ -1,17 +1,35 @@
 package com.aleanse.ifood.service;
 
 
+import com.aleanse.ifood.exception.EntidadeNaoEncontradaException;
+import com.aleanse.ifood.model.Cozinha;
 import com.aleanse.ifood.model.Restaurante;
+import com.aleanse.ifood.repository.CozinhaRepository;
 import com.aleanse.ifood.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CadastroRestauranteService {
     @Autowired
     private RestauranteRepository restauranteRepository;
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
 
     public Restaurante salvarRestaurante(Restaurante restaurante){
-        return restauranteRepository.save(restaurante);
+        Optional<Cozinha> cozinhaId = cozinhaRepository.findById(restaurante.getCozinha().getId());
+
+        if(cozinhaId.isEmpty()){
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe um cadastro de cozinha com código %d",restaurante.getCozinha().getId()));
+        }else {
+            restaurante.setCozinha(cozinhaId.get());
+            return restauranteRepository.save(restaurante);
+        }
+
+
+
     }
 }
