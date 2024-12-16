7b71a700c645815af02fcf7976513ad89687552f
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-12-15 23:47:48 -0300

    personalizando mensagem para HttpMessageNotReadable e InvalidFormatException

--- API-ifood-spring/src/main/java/com/aleanse/ifood/exceptionhandler/ApiExceptionHandler.java
+++ API-ifood-spring/src/main/java/com/aleanse/ifood/exceptionhandler/ApiExceptionHandler.java
@@ -2,19 +2,59 @@ package com.aleanse.ifood.exceptionhandler;
 
 import com.aleanse.ifood.exception.EntidadeEmUsoException;
 import com.aleanse.ifood.exception.EntidadeNaoEncontradaException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+        if (rootCause instanceof InvalidFormatException){
+            return handleInvalidFormatException((InvalidFormatException) rootCause,headers,status,request);
+        }
+        String detail = "o corpo da requisição está invalido, Verifique erro de sintaxe.";
+        Problem problema = Problem.builder()
+                .title("entidade não encontrada")
+                .status(status.value())
+                .detail(detail).build();
+        return super.handleExceptionInternal(ex, problema , headers, status, request);
+    }
+    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
+                                            HttpHeaders headers, HttpStatusCode status,
+                                            WebRequest request) {
+        ex.getPath().forEach(ref -> System.out.println(ref.getFieldName()));
+        String path = ex.getPath().stream()
+                .map(ref -> ref.getFieldName())
+                .collect(Collectors.joining("."));
+        String detail = String.format("a propriedade %s recebeu o valor %s," +
+                "que é de um tipo invalido. Corriga e informe um valor compátivel com o tipo %s.",
+                path,ex.getValue(),ex.getTargetType().getSimpleName());
+        Problem problem = Problem.builder()
+                .title("entidade não encontrada")
+                .status(status.value())
+                .detail(detail).build();
+
+        return super.handleExceptionInternal(ex,problem, headers,status,request);
+
+    }
+
+
+
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontrado(
             EntidadeNaoEncontradaException e){
@@ -31,7 +71,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> handleEntidadeEmUso(
+    public ResponseEntity<?> handleEntidadeEmUsoException(
             EntidadeEmUsoException e){
         HttpStatus status = HttpStatus.CONFLICT;
         Problem problema = Problem.builder()
